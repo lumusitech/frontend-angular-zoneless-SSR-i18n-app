c1cd402de6a8c35412ff077495cf1e1281ef36b7
Luciano Figueroa <lumusitech@gmail.com>
2025-02-14 12:41:45 -0300

    feat: add base language service

---
+++ src/app/services/language.service.ts
@@ -0,0 +1,14 @@
+import { inject, Injectable } from '@angular/core';
+import { SsrCookieService } from 'ngx-cookie-service-ssr';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LanguageService {
+  cookie = inject(SsrCookieService);
+
+  changeLang(lang: string) {
+    this.cookie.set('lang', lang);
+    //TODO: change language
+  }
+}
