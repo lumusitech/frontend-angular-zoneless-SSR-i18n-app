4344e73e8dd8143e264d71da47ef135a48152130
Luciano Figueroa <lumusitech@gmail.com>
2025-02-14 12:44:53 -0300

    feat: add change language method

--- src/app/components/language-selector/language-selector.component.ts
+++ src/app/components/language-selector/language-selector.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { LanguageService } from '../../services/language.service';
 
 @Component({
   selector: 'app-language-selector',
@@ -7,10 +8,19 @@ import { Component, signal } from '@angular/core';
   styles: ``,
 })
 export class LanguageSelectorComponent {
+  languageService = inject(LanguageService);
+
   languages = signal([
     { code: 'en', flag: '🇺🇸' },
     { code: 'es', flag: '🇪🇸' },
     { code: 'fr', flag: '🇫🇷' },
     { code: 'it', flag: '🇮🇹' },
   ]);
+
+  changeLanguage(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    const lang = target.value;
+
+    this.languageService.changeLang(lang);
+  }
 }
