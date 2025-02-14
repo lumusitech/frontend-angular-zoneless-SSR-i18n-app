b5cf03d08c865fb72cddbf2f45e9095376df8d97
Luciano Figueroa <lumusitech@gmail.com>
2025-02-14 12:46:57 -0300

    feat: add manual temporal test for get langauge selection into server and client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SsrCookieService } from 'ngx-cookie-service-ssr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  cookieService = inject(SsrCookieService);
+
+  cookieLogEffect = effect(() => {
+    console.log({ cookie: this.cookieService.get('lang') });
+  });
+}
