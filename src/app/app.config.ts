4641f3b76b0e1766f5c50000687b88f753107ddd
Luciano Figueroa <lumusitech@gmail.com>
2025-02-14 12:43:11 -0300

    chore: add config for translate library

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,42 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { SsrCookieService } from 'ngx-cookie-service-ssr';
 
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideHttpClient(withFetch()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: HttpClient,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    ),
+    // Cookies
+    SsrCookieService,
+  ],
 };
